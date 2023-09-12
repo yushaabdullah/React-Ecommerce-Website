80aa92866a4c646027fb3fbd0ec6a473731e12d1
unknown <yusha814@gmail.com>
2023-09-12 17:50:01 +0600

    category page and contact us added

---
+++ client/src/Components/Categories/Categories.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Categories.scss";
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <div className="col">
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/3496542/pexels-photo-3496542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <button>
+            <Link className="link" to="/products/1">
+              Sale
+            </Link>
+          </button>
+        </div>
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/4947273/pexels-photo-4947273.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <button>
+            <Link className="link" to="/products/1">
+              Women
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col">
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/1852300/pexels-photo-1852300.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <button>
+            <Link className="link" to="/products/1">
+              New Season
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col col-l">
+        <div className="row">
+          <div className="col">
+            <div className="row">
+              <img
+                src="https://images.pexels.com/photos/17488170/pexels-photo-17488170/free-photo-of-man-wearing-alternative-outfit.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <button>
+                <Link className="link" to="/products/1">
+                  Men
+                </Link>
+              </button>
+            </div>
+          </div>
+          <div className="col">
+            <div className="row">
+              <img
+                src="https://images.pexels.com/photos/1460838/pexels-photo-1460838.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
+              <button>
+                <Link className="link" to="/products/1">
+                  Accessories
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/1456737/pexels-photo-1456737.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+          />
+          <button>
+            <Link className="link" to="/products/1">
+              Shoes
+            </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Slider from "../../Components/Slider/Slider";
 import "./Home.scss";
 import FeaturedProducts from "../../Components/FeaturedProducts/FeaturedProducts";
+import Categories from "../../Components/Categories/Categories";
+import Contact from "../../Components/Contact/Contact";
 
 const Home = () => {
   return (
     <div className="home">
       <Slider />
       <FeaturedProducts type="Featured" />
+      <Categories />
       <FeaturedProducts type="Trending" />
+      <Contact />
     </div>
   );
 };
